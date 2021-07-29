f713290a1b07c0a3fa208b40ffdadb7bfe74aa0b
syme robinson <42970574+symeRobinson@users.noreply.github.com>
2021-07-29 12:28:25 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Syme Robinson ( roblobster#0001 )'
+description 'Free Server-Sided Sirens'
+version '1.0.0'
+
+files { 
+	"dlc_svsirens/custom_sirens.awc",
+	"data/svsirens_sounds.dat54.nametable",
+	"data/svsirens_sounds.dat54.rel"
+}
+
+data_file "AUDIO_WAVEPACK" "dlc_svsirens"
+data_file "AUDIO_SOUNDDATA" "data/svsirens_sounds.dat"
+
